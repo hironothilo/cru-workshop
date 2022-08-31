34e1463cf94de52c6fba6d3d3c764fb806744cfd
hironothilo <49652@cru.ac.th>
2022-08-31 13:00:28 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import { Fragment } from 'react';
+import React, { useState } from 'react';
 import  Form  from './component/Form';
 
 function App() {
+  const [item, setItem] = useState([]);
+  const addItem = (newItem) =>{
+    setItem((prevItem) => {
+      return [newItem, ...prevItem]
+    });
+  }
   return (
     <>
-      <Form/>
+      <Form addItem={addItem}/>
     </>
   );
 }
